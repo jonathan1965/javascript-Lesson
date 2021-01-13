4a4f36b59b89b68d6b57d6c2794f8682d2a85039
jonathan1965 <jonakarisa@gmail.com>
2021-01-13 09:53:02 +0200

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 //  Constants
 
- var interestRate= 0.3;
- interestRate= 1;
- console.log(interestRate)
+//  var interestRate= 0.3;
+//  interestRate= 1;
+//  console.log(interestRate)
+
+// primative type
+
+// let name = "Jonathan" // String Literal
+// let age = 30; // anumber Literal
+// let isApproved = true; // Boolean Literal
+// let firstName = undefined;
+// let lastName = null;  
+// let selectedColor= null;
+ 
+// reference Type
+
+
+
+
