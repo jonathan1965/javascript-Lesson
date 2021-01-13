8e2d53f8d81bbabcd2498b98327bbc3c7327d66b
jonathan1965 <jonakarisa@gmail.com>
2021-01-13 12:27:47 +0200

    make it better

---
+++ index1.js
@@ -0,0 +1,78 @@
+//  Constants
+
+//  var interestRate= 0.3;
+//  interestRate= 1;
+//  console.log(interestRate)
+
+// primative type
+
+// let name = "Jonathan" // String Literal
+// let age = 30; // anumber Literal
+// let isApproved = true; // Boolean Literal
+// let firstName = undefined;
+// let lastName = null;  
+// let selectedColor= null;
+ 
+// reference Type
+    // Object
+
+// let name= "Jonathan";
+// let age = 30;
+// let person = {
+//     name:"kalisa",
+//     age:30
+// }
+
+// let = person.age = 50;
+
+// // blacket notstion 
+
+// person['name'] = 'Maamy';
+
+// console.log(person['name'])
+
+// // other example of propertios
+
+// let selector = "name"
+// person[selector]="joghDo"
+// console.log(person['name'])
+
+
+
+
+//  Array  
+
+// let selectedColors = ['red','blue','']
+//    selectedColors [2]= "joka" 
+
+// console.log(selectedColors.length)
+
+
+// Function
+
+
+
+// function greet(name) {
+//      console.log('John Doe');
+// }
+
+// greet(); 
+
+
+// // 2.Performing atask
+// function greet(name,lastname) { // name is a parameters
+//      console.log('hello ' + name + ' ' + lastname);
+// }
+// greet('john','kalisa');  // John lastnameis an arguiment
+
+
+
+// 3.calculate a value
+
+// function square(number){
+//     return number*number;
+    
+// }
+ 
+//  console.log(square(2));
+
