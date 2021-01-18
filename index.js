cc95adf116be2a534452de19c60eb1492733048d
jonathan1965 <jonakarisa@gmail.com>
2021-01-18 15:01:50 +0200

    exercises 18

--- index.js
+++ index.js
@@ -310,20 +310,47 @@
 // }
 
 
-// exercises 17
+             // exercises 17
+
 // Multiple of 3:3,6,9
 // Multiple of 5:5,10
 
 
-console.log(sum(10));
+// console.log(sum(10));
 
 
-function sum(limit) {
-    let sum = 0;
+// function sum(limit) {
+//     let sum = 0;
+
+//     for (let i = 0; i <= limit; i++)
+//         if (i % 3 === 0 || i % 5 === 0)
+//             sum += i
+
+//     return sum;
+// }
+
+        // Exercises 18
+
+//  0-59: F
+//  60-69:D
+//  70-79:c
+//  80-89:B
+//  90-100:A
+
+
+const marks = [80,80,50]
+
+console.log(calculateGrade(marks));
+
+function calculateGrade(marks){
+    let sum = 0
+    for  (let mark of marks)
+    sum+= mark; 
     
-    for (let i = 0; i <= limit; i++)
-        if (i % 3 === 0 || i % 5 === 0)
-            sum += i
-
-    return sum;
+    let average = sum / marks.lenght;
+    if (average <60  ) return 'f'
+    if (average <70  ) return 'D'
+    if (average <80  ) return 'C'
+    if (average <90  ) return 'B'
+    else return 'A'
 }
