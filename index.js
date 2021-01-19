1473e988c4963104c075d3e091c352b8747bfbb7
jonathan1965 <jonakarisa@gmail.com>
2021-01-19 12:33:27 +0200

    Factory Function

--- index.js
+++ index.js
@@ -9,8 +9,27 @@
      },
      isVisible: true,
      draw: function() {
-          console.log('draw');
+        //   console.log('draw');
      }
  };
 
  circle.draw(); // Method 
+
+
+//    factory function
+
+function createCircle(radius) {
+    return {
+        radius,
+        draw() {
+             console.log('draw');
+        }
+    };
+}
+
+
+const Circle1 = createCircle(1);
+console.log(Circle1.draw()) 
+const Circle2= createCircle(20);
+console.log(Circle2)
+
