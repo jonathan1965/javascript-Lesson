07eb2a6a24db5869248cfd306287d3c85e79d46d
jonathan1965 <jonakarisa@gmail.com>
2021-01-19 13:13:00 +0200

    Dynamic Nature Object

--- index.js
+++ index.js
@@ -1,47 +1,54 @@
   //  Object-oriented Programming (OOP)
     
    
- const circle = {
-     radius: 1,
-     location: {
-         x:1,
-         y:1
-     },
-     isVisible: true,
-     draw: function() {
-        //   console.log('draw');
-     }
- };
+//  const circle = {
+//      radius: 1,
+//      location: {
+//          x:1,
+//          y:1
+//      },
+//      isVisible: true,
+//      draw: function() {
+//         //   console.log('draw');
+//      }
+//  };
 
- circle.draw(); // Method 
+ //circle.draw(); // Method 
 
 
 //    factory function
 
-function createCircle(radius) {
-    return {
-        radius,
-        draw() {
-             console.log('draw');
-        }
-    };
-}
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw() {
+//              console.log('draw');
+//         }
+//     };
+// }
 
 
-const Circle1 = createCircle(1);
-console.log(Circle1.draw()) 
-const Circle2= createCircle(20);
-console.log(Circle2)
+// const Circle1 = createCircle(1);
+// console.log(Circle1.draw()) 
+// const Circle2= createCircle(20);
+// console.log(Circle2)
 
 
                //Factory Function
 
-function Circle(radius){
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }
-}
+// function Circle(radius){
+//     this.radius = radius;
+//     this.draw = function () {
+//         console.log('draw');
+//     }
+// }
 
-const circle = new Circle(1)
-console.log(circle);
+// const circle = new Circle(1)
+// console.log(circle);
+
+const triangle = {
+    radius:1
+}
+triangle.p= 15;
+triangle.draw= function () {}
+console.log(triangle)
