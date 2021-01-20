59c76d673c2e48b2483bc2178e76754a8cb614dc
jonathan1965 <jonakarisa@gmail.com>
2021-01-20 16:13:02 +0200

    array

--- index.js
+++ index.js
@@ -62,3 +62,19 @@
         
 //  let course = courses.findIndex(course =>  course.name == "a" )
 // console.log(course)
+
+                  //  remove ellement from an array
+
+  const numbers  = [1,2,3,4]       
+
+  //End 
+  const last = numbers.pop()
+console.log(numbers)
+console.log(last)
+
+//   beggining 
+  numbers.shift()
+  numbers.splice()
+
+// Middle 
+numbers.splice(2)
