ec3b23fd76e183118e14ed037d71545dca9acc31
jonathan1965 <jonakarisa@gmail.com>
2021-01-21 10:04:50 +0200

    array

--- index.js
+++ index.js
@@ -103,13 +103,24 @@
 
         //   combine and slicing Array
 
-const first = [1,2,3];
-const second = [4,5,6,7];
+// const first = [1,2,3];
+// const second = [4,5,6,7];
 
-const combined = first.concat(second)
+// const combined = first.concat(second)
+// // slice annaray into two part  
+
+// const slice = combined.slice(2,4)
+// console.log(slice)
+
+
+                 //   The Spread Operator Array
+
+
+                 // const first = [1,2,3];
+const second = [4,5,6,7];
+const first = [1,2,3];
+const combined = [...first,...second]
 // slice annaray into two part  
 
-const slice = combined.slice(2,4)
-console.log(slice)
-
-
+const copy =  [...combined]
+console.log(copy)
