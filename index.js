f9159c1f51a0604f29c8ac9102799195f6e574b0
jonathan1965 <jonakarisa@gmail.com>
2021-01-22 23:42:38 +0200

    array

--- index.js
+++ index.js
@@ -149,28 +149,44 @@
 
              // Sorting an Array
             
-const numbers = [2,3,1];
- const sorting = numbers.sort();
- console.log(sorting)
- numbers.reverse();
- console.log(numbers)
+// const numbers = [2,3,1];
+//  const sorting = numbers.sort();
+//  console.log(sorting)
+//  numbers.reverse();
+//  console.log(numbers)
 
- const courses = [
-     {id:  1, name:'Node.js'},
-     {id:  2, name:'Javascript'}
- ];
+//  const courses = [
+//      {id:  1, name:'Node.js'},
+//      {id:  2, name:'Javascript'}
+//  ];
 
-courses.sort(function(a,b){
-    // a < b => 1
-    // a > b => 1
-    // a === b => 0
- const nameA = a.name.toUpperCase();
- const nameB = b.name.toUpperCase();
+// courses.sort(function(a,b){
+//     // a < b => 1
+//     // a > b => 1
+//     // a === b => 0
+//  const nameA = a.name.toUpperCase();
+//  const nameB = b.name.toUpperCase();
 
-    if (nameA < nameB) return -1
-    if (nameA > nameB) return 1;
-    return 0;
+//     if (nameA < nameB) return -1
+//     if (nameA > nameB) return 1;
+//     return 0;
+// })
+// console.log(courses);
+
+
+                  // Testing The Element an Array
+
+const numbers = [1,-1,2,3];
+
+
+// every ()
+// some()
+const isPositive = numbers.every( function (value) {
+     return value >= 0;
 })
-console.log(courses);
 
+const atLeastOnePositive = numbers.some(function(value){
+    return value >=0;
+})
 
+console.log(isPositive)
