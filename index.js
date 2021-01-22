9920cb50c03864ea0f0a7416667efa8f92054f1c
jonathan1965 <jonakarisa@gmail.com>
2021-01-22 23:26:13 +0200

    array

--- index.js
+++ index.js
@@ -138,14 +138,39 @@
 
             //  joining an Array
 
-const numbers = [1,2,3];
-const joined = numbers.join(','); 
-console.log(joined);
-const message = 'This is my first  message'
+// const numbers = [1,2,3];
+// const joined = numbers.join(','); 
+// console.log(joined);
+// const message = 'This is my first  message'
+// const parts = message.split(' ')
+// const combined = parts.join('-')
+// console.log(combined)
 
-const parts = message.split(' ')
-const combined = parts.join('-')
-console.log(combined)
 
+             // Sorting an Array
+            
+const numbers = [2,3,1];
+ const sorting = numbers.sort();
+ console.log(sorting)
+ numbers.reverse();
+ console.log(numbers)
+
+ const courses = [
+     {id:  1, name:'Node.js'},
+     {id:  2, name:'Javascript'}
+ ];
+
+courses.sort(function(a,b){
+    // a < b => 1
+    // a > b => 1
+    // a === b => 0
+ const nameA = a.name.toUpperCase();
+ const nameB = b.name.toUpperCase();
+
+    if (nameA < nameB) return -1
+    if (nameA > nameB) return 1;
+    return 0;
+})
+console.log(courses);
 
 
