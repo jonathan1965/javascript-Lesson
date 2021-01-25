8db9b9f897051ea012db6aeacc078347db6b775a
jonathan1965 <jonakarisa@gmail.com>
2021-01-25 21:17:18 +0200

    exercises 7

--- index.js
+++ index.js
@@ -326,16 +326,28 @@
 // }
 
 
+          //   Exercises 6 get the maximum number
+
 const numbers = [1,2,3,4]
 
-const max = getMax([0])
+const max = getMax(numbers)
 
 console.log(max)
 
 function getMax(array) {
-        if (array.length === 0)
-        return undefined;
+        if (array.length === 0) return undefined;
+        // let max = array[0];
+
+        // for (let i=1 ; i< array.length ; i++ )
+        // if (array[i] > max)
+        // max = array[i]
+
+        // return max;
+
+    return     array.reduce((a,b) =>  (a > b) ? a : b)
+
 }
+
 
 
 
