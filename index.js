44da283e989cb3fe12a48223effc195e95d4a06a
jonathan1965 <jonakarisa@gmail.com>
2021-01-27 23:36:38 +0200

    function

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-// function Declaration
+// // function Declaration
 
+// function walk() {
+//     console.log('walk')
+// }
+// // Named functioN expression
+// let run = function walk() {
+//    console.log('run');
+// };
+// // Anonymous  functioN expression
+// let run1 = function () {
+//     console.log('run me');
+//  };
+//  run1();
+//  let move = run1;
+//  move();
+
+
+
+
+
+         // function Declaration
+
+ walk();
 function walk() {
     console.log('walk')
+
 }
-
-// Named functioN expression
-
+        // Named functioN expression
+run();  //can run
 let run = function walk() {
-   console.log('run');
-};
-
-
-// Anonymous  functioN expression
-let run1 = function () {
-    console.log('run me');
+    console.log('run');
  };
- 
- run1();
- let move = run1;
-
- move();
+ // Anonymous  functioN expression
+ let run1 = function () {
+     console.log('run me');
+  };
+  run1();
+  let move = run1;
+  move();
