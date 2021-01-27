39f61f703b727190cdb7d82018e88c339a2bb3e6
jonathan1965 <jonakarisa@gmail.com>
2021-01-27 23:48:53 +0200

    function

--- index.js
+++ index.js
@@ -17,24 +17,42 @@
 
 
 
-
+        //   Hooisting
 
          // function Declaration
 
- walk();
-function walk() {
-    console.log('walk')
+//  walk();
+// function walk() {
+//     console.log('walk')
 
+// }
+//         // Named functioN expression
+// run();  //can run
+// let run = function walk() {
+//     console.log('run');
+//  };
+//  // Anonymous  functioN expression
+//  let run1 = function () {
+//      console.log('run me');
+//   };
+//   run1();
+//   let move = run1;
+//   move();
+
+
+
+
+// function sum (a,b){
+//     return a+b;
+// }
+// console.log(sum(1,5 ))
+
+
+function sum1 (a,b){
+    let total = 0;
+    for (let value of arguments)
+      total += value;
+    return total
+    
 }
-        // Named functioN expression
-run();  //can run
-let run = function walk() {
-    console.log('run');
- };
- // Anonymous  functioN expression
- let run1 = function () {
-     console.log('run me');
-  };
-  run1();
-  let move = run1;
-  move();
+console.log(sum1(1,5,2))
